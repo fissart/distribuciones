eefc3e38e08e6b4fe2b53b098d3bb338f8f05899
ricardofisma <ricardomallqui6@gmail.com>
2020-04-18 13:16:49 -0500

    w

--- js/distributionDisplay.js
+++ js/distributionDisplay.js
@@ -4,150 +4,157 @@ var plotOptions = [];
 var sliderPoints = 101;
 var roundDigits = 3;
 var updateTime = 100;
-var areaFillColor="rgb(209, 104, 58)";
+var areaFillColor = "#4EC5F1";
 
-plotOptions["pdf"] = { 
-				yaxis: { min:0 }, 
-				colors: ["rgb(50, 86, 121)", "rgb(0, 62, 200)","rgb(150,150,150)","rgb(150,150,150)", "rgb(0,200,0)"], 
-				series: {
-					points: { fillColor: "rgb(200, 0, 15)"}
-					},
-				grid: { hoverable: false, clickable: true },
-				crosshair: { mode: "x" }
-				};
-plotOptions["cdf"] = { 
-				yaxis: { min:0, max: 1.001},
-				colors: ["rgb(0,0,0)", "rgb(0, 8, 200)", "rgb(150,150,150)","rgb(150,150,150)", "rgb(0,200,0)"],
-				series: {
-					points: { fillColor: "rgb(0,200,0)"}
-					},
-				grid: { hoverable: true, clickable: false }
-				};
+plotOptions["pdf"] = {
+  yaxis: { min: 0 },
+  colors: [
+    "rgb(0,20,0)",
+    "rgb(200,0,0)",
+    "rgb(0,1,0)",
+    "rgb(150,150,150)",
+    "rgb(0,20,0)",
+  ],
+  series: {
+    points: { fillColor: "rgb(0,200,0)" },
+  },
+  grid: { hoverable: true, clickable: true },
+  crosshair: { mode: "x" },
+};
+plotOptions["cdf"] = {
+  yaxis: { min: 0, max: 1.001 },
+  colors: [
+    "rgb(0,0,0)",
+    "rgb(200,0,0)",
+    "rgb(150,150,150)",
+    "rgb(150,150,150)",
+    "rgb(0,20,0)",
+  ],
+  series: {
+    points: { fillColor: "rgb(0,200,0)" },
+  },
+  grid: { hoverable: true, clickable: false },
+};
 
 var plot;
 var dist;
 var distName;
 var ptznNum;
-var starts; 
+var starts;
 var rangesHi;
 var rangesLo;
 var plotxrng;
 
 var distributions = [];
 
-function getShaded(plot,pos, limits, cumdens, args){
-		var data = plot.getData();
-		var x1 = data[2];
-		var x2 = data[3];
+function getShaded(plot, pos, limits, cumdens, args) {
+  var data = plot.getData();
+  var x1 = data[2];
+  var x2 = data[3];
 
-		if(x2.data.length!=0){
-		 	x2 = x2.data[0][0];
-		 	x1 = x1.data[0][0];
-		 	var minx = Math.min(x1,x2);
-		 	var maxx = Math.max(x1,x2);
-		 	return cumdens.apply(null, [maxx].concat(args) ) - 
-		 		   cumdens.apply(null, [minx].concat(args) );
-		 }else if(x1.data.length!=0){
-			x1 = x1.data[0][0];
-			return cumdens.apply(null, [x1].concat(args) );
-		 }else{
-			return cumdens.apply(null, [pos.x].concat(args) );
-			x2 = limits[0];
-			x1 = pos.x;
-		 }
-
+  if (x2.data.length != 0) {
+    x2 = x2.data[0][0];
+    x1 = x1.data[0][0];
+    var minx = Math.min(x1, x2);
+    var maxx = Math.max(x1, x2);
+    return (
+      cumdens.apply(null, [maxx].concat(args)) -
+      cumdens.apply(null, [minx].concat(args))
+    );
+  } else if (x1.data.length != 0) {
+    x1 = x1.data[0][0];
+    return cumdens.apply(null, [x1].concat(args));
+  } else {
+    return cumdens.apply(null, [pos.x].concat(args));
+    x2 = limits[0];
+    x1 = pos.x;
+  }
 }
-
 
 function showTooltip(x, y, contents) {
-    $('<div id="tooltip">' + contents + '</div>').css( {
-        position: 'absolute',
-        display: 'none',
-        top: y - 30,
-        left: x + 10,
-        border: '1px solid #b9322b',
-        padding: '20px',
-        'background-color': '#2b96b9',
-        opacity: 0.80,
-        'font-family': 'sans-serif'
-    }).appendTo("body").fadeIn(0);
+  $('<div id="tooltip">' + contents + "</div>")
+    .css({
+      position: "absolute",
+      display: "none",
+      top: y - 30,
+      left: x + 10,
+      border: "1px solid #000",
+      padding: "3px",
+      "background-color": "#fe5e",
+      opacity: 0.9,
+      "font-family": "serif",
+      "border-radius": "5px",
+    })
+    .appendTo("body")
+    .fadeIn(0);
 }
 
-function makeSortedTable(type, caption){
+function makeSortedTable(type, caption) {
+  var n = distributions.length;
+  var list = [];
+  var i = 0;
+  for (d in distributions) {
+    if (distributions.hasOwnProperty(d)) {
+      if (distributions[d].type == type || type == "master") list.push(d);
+    }
+  }
 
-	var n = distributions.length;
-	var list = [];
-	var i=0;
-    for(d in distributions){
-    	if (distributions.hasOwnProperty(d))
-	   	{
-			if(distributions[d].type == type || type == "master") list.push(d);
-        }
-	}
-	
-	list.sort();
-	
-	document.id(type+'_distributionsdiv').grab(
-        new Element("table",{
-            id: type+"_distributiontable", 
-            'class':"distribution"
-        })
-    );
-    
-    var cap = new Element('caption', {
-		html: caption
-	});
-    document.id(type+'_distributiontable').grab(cap);
+  list.sort();
 
-    refTableHeaderRow(type);
-    
-    var body = new Element("tbody",{
-        id: type+"_distributiontablebody"
-    });
-    document.id(type+'_distributiontable').grab(body);
-	
-	
-	if(list.length==0) return;
-	
-	for(;i<list.length;i++)
-		distributions[list[i]].refTableRow(i%2==1,type);
-	
+  document.id(type + "_distributionsdiv").grab(
+    new Element("table", {
+      id: type + "_distributiontable",
+      class: "distribution",
+    })
+  );
+
+  var cap = new Element("caption", {
+    html: caption,
+  });
+  document.id(type + "_distributiontable").grab(cap);
+
+  refTableHeaderRow(type);
+
+  var body = new Element("tbody", {
+    id: type + "_distributiontablebody",
+  });
+  document.id(type + "_distributiontable").grab(body);
+
+  if (list.length == 0) return;
+
+  for (; i < list.length; i++)
+    distributions[list[i]].refTableRow(i % 2 == 1, type);
 }
 
+function makePDF() {
+  $("#buttonCDF").removeAttr("disabled");
+  $("#buttonPDF").attr("disabled", "disabled");
 
-function makePDF()
-{
-	$("#buttonCDF").removeAttr("disabled");
-	$("#buttonPDF").attr('disabled','disabled');
-	
-	//plot = false;
-	dist.updatePlot(ptznNum);
+  //plot = false;
+  dist.updatePlot(ptznNum);
 }
 
-function makeCDF()
-{
-	$("#buttonCDF").attr('disabled','disabled');
-	$("#buttonPDF").removeAttr("disabled");
-	
-	//plot = false;
-	dist.updatePlot(ptznNum);
+function makeCDF() {
+  $("#buttonCDF").attr("disabled", "disabled");
+  $("#buttonPDF").removeAttr("disabled");
+
+  //plot = false;
+  dist.updatePlot(ptznNum);
 }
 
-function slide(name, ui){
-	dist.updatePlot(ptznNum,name, ui);
+function slide(name, ui) {
+  dist.updatePlot(ptznNum, name, ui);
 }
 
-function is_int(value){ 
-  if((parseFloat(value) == parseInt(value)) && !isNaN(value)){
-      return true;
-  } else { 
-      return false;
-  } 
+function is_int(value) {
+  if (parseFloat(value) == parseInt(value) && !isNaN(value)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function round(x,digits)
-{
-	var factor = Math.pow( 10, digits );
-	return Math.round( x * factor ) / factor;
+function round(x, digits) {
+  var factor = Math.pow(10, digits);
+  return Math.round(x * factor) / factor;
 }
-
